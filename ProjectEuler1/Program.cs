19f2634ad0b5cb01186500fe626defde9d550232
Tory Kepler <t.kepler@pacificcolumns.com>
2022-06-18 12:11:05 -0500

    Solved problem 1

--- ProjectEuler1/Program.cs
+++ ProjectEuler1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var watch = new System.Diagnostics.Stopwatch();
+
+watch.Start();
+
+int sum = 0;
+
+for (int i = 0; i < 1000; i++)
+{    
+    if(i % 3 == 0 || i % 5 == 0)
+    {
+        sum+= i;
+    }
+}
+
+Console.WriteLine(sum);
+
+watch.Stop();
+
+Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
