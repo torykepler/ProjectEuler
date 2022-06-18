06c20dff85d277e78350676db435760228082ba4
Tory Kepler <t.kepler@pacificcolumns.com>
2022-06-18 12:55:21 -0500

    Solved problem 2

---
+++ ProjectEuler2/Program.cs
@@ -0,0 +1,29 @@
+﻿var watch = new System.Diagnostics.Stopwatch();
+
+watch.Start();
+
+Console.WriteLine(Fibonacci(1, 2, 4000000, 2));
+
+static int Fibonacci(int x, int y, int max, int sum)
+{
+    int nextNumber = x + y;
+
+    if(nextNumber > max)
+    {
+        return sum;
+    }
+    else
+    {
+        if (nextNumber % 2 == 0)
+        {
+            sum += nextNumber;
+        }
+
+        return Fibonacci(y, nextNumber, max, sum);
+    }
+}
+
+
+watch.Stop();
+
+Console.WriteLine($"Execution Time: {watch.ElapsedMilliseconds} ms");
