828ede935e12bbedd6a6078912c9b7d95d602a06
Tory Kepler <t.kepler@pacificcolumns.com>
2022-06-18 13:09:58 -0500

    Added problem information for problems 1 & 2

--- ProjectEuler1/Program.cs
+++ ProjectEuler1/Program.cs
@@ -1,4 +1,13 @@
-﻿var watch = new System.Diagnostics.Stopwatch();
+﻿/* https://projecteuler.net/problem=1
+ * 
+ * Title: Multiples of 3 or 5
+ * 
+ * Description: If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+var watch = new System.Diagnostics.Stopwatch();
 
 watch.Start();
 
